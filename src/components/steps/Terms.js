fed72d21e5e66a5a08a4d48a9dd0184ccd52edb9
Emmanuel Etukudo <emzzyyoung@gmail.com>
2021-09-04 21:46:13 +0100

    update on steps

--- src/components/steps/Terms.js
+++ src/components/steps/Terms.js
@@ -1,10 +1,20 @@
 import react from 'react';
 
-const Terms  = () => {
-    return(
-        <p>
-        This simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
-    </p>
+const Terms = () => {
+    return (
+        <div>
+            <p>
+                This simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not
+                only five centuries, but also the leap into electronic typesetting,
+                remaining essentially unchanged. It was popularised in the 1960s with
+                the release of Letraset sheets containing Lorem Ipsum passages, and more
+                recently with desktop publishing.
+            </p>
+            <div class="form-group form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+                <label class="form-check-label" for="exampleCheck1">Accept</label>
+            </div>
+        </div>
     )
 }
 
