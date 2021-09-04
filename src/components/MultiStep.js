06398357ad310911482059bdc3434fd64c3791ab
Emmanuel Etukudo <emzzyyoung@gmail.com>
2021-09-04 18:42:25 +0100

    update on multi step component

--- src/components/MultiStep.js
+++ src/components/MultiStep.js
@@ -1,11 +1,20 @@
 import react from 'react';
+import { About, Terms, ConcentForm } from './steps/steps';
+import StepZilla from "react-stepzilla";
 
+const steps =
+    [
+        { name: 'About', component: <About /> },
+        { name: 'Terms', component: <Terms /> },
+        { name: 'Concent', component: <ConcentForm /> },
+    ]
 
 const MultiSteps = () => {
 
-    return(
-        <>
-        </>
+    return (
+        <div className='step-progress'>
+            <StepZilla steps={steps} />
+        </div>
     );
 }
 
