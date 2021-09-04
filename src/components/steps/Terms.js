1eed5649df58cc5147b517e33c673e33c705f3d5
Emmanuel Etukudo <emzzyyoung@gmail.com>
2021-09-04 21:43:40 +0100

    update on parent component

--- src/components/steps/Terms.js
+++ src/components/steps/Terms.js
@@ -2,8 +2,9 @@ import react from 'react';
 
 const Terms  = () => {
     return(
-        <>
-        </>
+        <p>
+        This simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
+    </p>
     )
 }
 
